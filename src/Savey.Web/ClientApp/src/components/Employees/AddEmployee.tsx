d298f0c39a350e8a42810f9f3d328c5bf5eccf96
FERNANDO NANDO CHACON MEDINA <marco.chacon@unitec.edu>
2019-06-08 16:21:17 -0600

    adding employee fields

--- src/Savey.Web/ClientApp/src/components/Employees/AddEmployee.tsx
+++ src/Savey.Web/ClientApp/src/components/Employees/AddEmployee.tsx
@@ -1,4 +1,5 @@
-﻿import React, { Component } from "react";
+﻿
+import React, { Component } from "react";
 
 import {
   Form,
@@ -11,7 +12,8 @@ import {
   Col,
   Checkbox,
   Button,
-  AutoComplete
+  AutoComplete,
+  DatePicker
 } from "antd";
 import { FormComponentProps } from "antd/lib/form";
 
@@ -116,31 +118,19 @@ class AddEmployee extends Component<Props, {}> {
 
     return (
       <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item label="E-mail">
-          {getFieldDecorator("email", {
+        <Form.Item label={<span>Primer Nombre&nbsp;</span>}>
+          {getFieldDecorator("firstName", {
             rules: [
               {
-                type: "email",
-                message: "The input is not valid E-mail!"
-              },
-              {
                 required: true,
-                message: "Please input your E-mail!"
+                message: "Inserte su Primer Nombre!",
+                whitespace: true
               }
             ]
           })(<Input />)}
         </Form.Item>
-        <Form.Item
-          label={
-            <span>
-              Nickname&nbsp;
-              <Tooltip title="What do you want others to call you?">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>
-          }
-        >
-          {getFieldDecorator("nickname", {
+        <Form.Item label={<span>Segundo Nombre&nbsp;</span>}>
+          {getFieldDecorator("middleName", {
             rules: [
               {
                 required: true,
@@ -150,57 +140,34 @@ class AddEmployee extends Component<Props, {}> {
             ]
           })(<Input />)}
         </Form.Item>
-        <Form.Item label="Habitual Residence">
-          {getFieldDecorator("residence", {
-            initialValue: ["zhejiang", "hangzhou", "xihu"],
+        <Form.Item label={<span>Primer Apellido&nbsp;</span>}>
+          {getFieldDecorator("surname", {
             rules: [
               {
-                type: "array",
                 required: true,
-                message: "Please select your habitual residence!"
+                message: "Please input your surname!",
+                whitespace: true
               }
             ]
-          })(<Cascader options={residences} />)}
+          })(<Input />)}
         </Form.Item>
-        <Form.Item label="Phone Number">
-          {getFieldDecorator("phone", {
+        <Form.Item label={<span>Segundo Apellido&nbsp;</span>}>
+          {getFieldDecorator("secondSurname", {
             rules: [
-              { required: true, message: "Please input your phone number!" }
+              {
+                required: true,
+                message: "Please input your second surname!",
+                whitespace: true
+              }
             ]
-          })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
+          })(<Input />)}
         </Form.Item>
-        <Form.Item
-          label="Captcha"
-          extra="We must make sure that your are a human."
-        >
-          <Row gutter={8}>
-            <Col span={12}>
-              {getFieldDecorator("captcha", {
-                rules: [
-                  {
-                    required: true,
-                    message: "Please input the captcha you got!"
-                  }
-                ]
-              })(<Input />)}
-            </Col>
-            <Col span={12}>
-              <Button>Get captcha</Button>
-            </Col>
-          </Row>
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          {getFieldDecorator("agreement", {
-            valuePropName: "checked"
-          })(
-            <Checkbox>
-              I have read the <a href="">agreement</a>
-            </Checkbox>
-          )}
-        </Form.Item>
+            <Form.Item label="Fecha inicio empresa">
+                <DatePicker />
+                </Form.Item >
         <Form.Item {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit">
-            Register
+            Registro
           </Button>
         </Form.Item>
       </Form>
